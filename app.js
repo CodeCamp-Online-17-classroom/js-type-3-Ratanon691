505d2d9b42601d4c74c679a0f98e157e62df5ab9
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-20 12:28:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// write code here
+let a = undefined;
+let b = ' ';
+let c = !b;
+
+let d = Boolean(a)
+let e = Boolean(b)
+let f = Boolean(c)
+console.log(d,e,f)
+//d = false, e = true, f = false
+//undefined เป็นfalse
+//stringเปล่าเป็นtrue
+//เพราะมี ! เลยเป็น false
